2374086cf07941e1426edcb689acb3e589305bea
Jenifer Alvarez <jniferalvarez@gmail.com>
2023-05-10 17:35:21 -0300

    actualizacion de script header

--- js/header.js
+++ js/header.js
@@ -1,35 +1,35 @@
 let headerCAD = `
-<nav class="main-nav" id="main-nav">
+<nav class="barra-nav" id="barra-nav">
 
 <div class="container container-flex">
+
+
+    <div class="boton-nav">
 
     <div class="main-header-container">
         <img src="./img/LOGO.png" alt="Logo" width="200" height="200">
         <h1 class="main-header-title">
-        <span class="word1">Refugio</span><br>
-        <span class="word2">Huellitas</span><br>
-        <span class="word3">de Amor</span>
+        <span class="palabra1">Refugio</span><br>
+        <span class="palabra2">Huellitas</span><br>
+        <span class="palabra3">de Amor</span>
         </h1>
     </div>
-
-    <tbody>
-        <tr>
-            <td>
-                <a href="index.html">
-                    <img src="./img/1.ico" alt="">
-                </a>
-                <a href="comunidad.html">
-                   <img src="./img/2.ico" alt="">
-                </a>
-                <a href="apoyo.html">
-                    <img src="./img/3.ico" alt="">
-                </a>
-                <a href="contacto.html">
-                    <img src="./img/4.ico" alt="">
-                </a>
-            </td>
-        </tr>
-    </tbody>
+            
+    <a href="index.html">
+    <img src="./img/1.ico" alt="">
+    </a>
+    <a href="comunidad.html">
+    <img src="./img/2.ico" alt="">
+    </a>
+    <a href="apoyo.html">
+    <img src="./img/3.ico" alt="">
+    </a>
+    <a href="contacto.html">
+    <img src="./img/4.ico" alt="">
+    </a>
+        
+        
+    </div>
 
 </div>
 
