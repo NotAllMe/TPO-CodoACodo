8e01cd4b4df36d1bce8085641afd03fd13794413
Nachocal <130698597+Nachocal@users.noreply.github.com>
2023-05-12 23:39:31 -0300

    Add files via upload
    Fix de Header (botones y logo nuevo)

--- js/header.js
+++ js/header.js
@@ -1,35 +1,34 @@
 let headerCAD = `
 <nav class="barra-nav" id="barra-nav">
 
-<div class="container container-flex">
+<div class="container-flex">
 
 
-    <div class="boton-nav">
+   <div class="divlogo">
 
-    <div class="main-header-container">
-        <img src="./img/LOGO.png" alt="Logo" width="200" height="200">
-        <h1 class="main-header-title">
-        <span class="palabra1">Refugio</span><br>
-        <span class="palabra2">Huellitas</span><br>
-        <span class="palabra3">de Amor</span>
-        </h1>
-    </div>
-            
-    <a href="index.html">
+   <a href="index.html">
+    <img src="./img/LOGOpoppins.svg" alt="Logo" height="200">
+   </a>
+
+   </div>
+
+    
+    <div class="divbotonheader">        
+    <a class="botonheader" href="adopcion.html">
     <img src="./img/1.ico" alt="">
     </a>
-    <a href="comunidad.html">
+    <a class="botonheader" href="comunidad.html">
     <img src="./img/2.ico" alt="">
     </a>
-    <a href="apoyo.html">
+    <a class="botonheader" href="apoyo.html">
     <img src="./img/3.ico" alt="">
     </a>
-    <a href="contacto.html">
+    <a class="botonheader" href="contacto.html">
     <img src="./img/4.ico" alt="">
     </a>
-        
-        
     </div>
+        
+        
 
 </div>
 
