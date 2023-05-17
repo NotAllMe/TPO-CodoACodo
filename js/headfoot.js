7eed9de747b5d283fbc69771eed879eaeb05b9cd
David Ledesma <ledesma@outlook.com.ar>
2023-05-17 16:15:16 -0300

    Limpieza 1 - JS, CSS, fuente.

---
+++ js/headfoot.js
@@ -0,0 +1,56 @@
+let headerCAD = `
+    <nav class="barra-nav" id="barra-nav">
+        <div class="container-flex">
+            <div class="divlogo">
+                <a href="index.html">
+                    <img src="./img/LOGOpoppins.svg" alt="Logo" height="200">
+                </a>
+            </div>
+            <div class="divbotonheader">
+                <a class="botonheader" href="adopcion.html">
+                    <img src="./img/1.ico" alt="">
+                </a>
+                <a class="botonheader" href="comunidad.html">
+                    <img src="./img/2.ico" alt="">
+                </a>
+                <a class="botonheader" href="apoyo.html">
+                    <img src="./img/3.ico" alt="">
+                </a>
+                <a class="botonheader" href="contacto.html">
+                    <img src="./img/4.ico" alt="">
+                </a>
+            </div>
+        </div>
+    </nav>
+`
+
+
+document.querySelector(".main-header").innerHTML = headerCAD;
+
+let footerCAD = `
+<div class="footer-sections">
+            <section class="footer-section">
+                <h2 class="footer-title">Enlaces de Interés</h2>
+                <a href="https://www.instagram.com/"><p class="footer-txt">Instagram</p></a>
+                <a href="https://www.facebook.com/"><p class="footer-txt">Facebook</p></a>
+                <a href="https://www.youtube.com/"><p class="footer-txt">Youtube</p></a>
+            </section>
+            <section class="footer-section">
+                <h2 class="footer-title">Contacto</h2>
+                <p class="footer-txt">Telefono: +5491100303456</p>
+                <p class="footer-txt">Correo: refugiohuellitasdeamor@corrreo.com</p>
+            </section>
+            <section class="footer-section">
+                <h2 class="footer-title">Suscribete al Boletín Mensual</h2>
+                <p class="footer-txt">Entérate antes de todas las Novedades</p>
+            </section>
+            <section class="footer-section">
+                <h2 class="footer-title">Politicas de Privacidad</h2>
+                <p class="footer-txt">Cookies</p>
+                <p class="footer-txt">Terminos y Condiciones</p>
+            </section>
+        </div>
+        <p class="copy">Refugio Huellitas de Amor - © 2023 - Todos los Derechos Reservados</p>
+        `
+
+document.querySelector(".main-footer").innerHTML = footerCAD;
