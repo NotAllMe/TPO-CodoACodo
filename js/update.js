bf562a3c429a69928742cce07c0bdb8820805ff3
jniferalvarez <128490001+jniferalvarez@users.noreply.github.com>
2023-07-01 20:10:23 -0300

    CREACION CRUD

---
+++ js/update.js
@@ -0,0 +1,60 @@
+console.log(location.search)     // lee los argumentos pasados a este formulario
+var id=location.search.substr(4)  // producto_update.html?id=1
+console.log(id)
+const { createApp } = Vue
+  createApp({
+    data() {
+      return {
+        id:0,
+        nombre:"",
+        imagen:"",
+        edadmeses:"",
+        sexo:"",
+        url:'http://tfohuellitas.pythonanywhere.com/mascotas/'+id,
+       }  
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    this.id=data.id
+                    this.nombre = data.nombre,
+                    this.imagen=data.imagen,
+                    this.sexo=data.sexo,
+                    this.edadmeses=data.edadmeses                    
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error=true              
+                })
+        },
+        modificar() {
+            let mascota = {
+                nombre:this.nombre,
+                edadmeses: this.edadmeses,
+                sexo: this.sexo,
+                imagen: this.imagen
+            }
+            var options = {
+                body: JSON.stringify(mascota),
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow'
+            }
+            fetch(this.url, options)
+                .then(function () {
+                    alert("Registro modificado")
+                    window.location.href = "./admin_update.html"; // navega a productos.html          
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al Modificar")
+                })      
+        }
+    },
+    created() {
+        this.fetchData(this.url)
+    },
+  }).mount('#app')
