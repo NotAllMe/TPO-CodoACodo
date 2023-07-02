11db77c1f4c4e6c300d666ef252e4c32075c8c0a
Notallme <100038680+Notallme@users.noreply.github.com>
2023-07-01 21:05:17 -0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -5,7 +5,7 @@ const { createApp } = Vue
         mascotas:[],
         //url:'http://localhost:5000/productos', 
    // si el backend esta corriendo local  usar localhost 5000(si no lo subieron a pythonanywhere)
-        url:'http://tfohuellitas.pythonanywhere.com/mascotas',   // si ya lo subieron a pythonanywhere
+        url:'https://tfohuellitas.pythonanywhere.com/mascotas',   // si ya lo subieron a pythonanywhere
         error:false,
         cargando:true,
         /*atributos para el guardar los valores del formulario */
