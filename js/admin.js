efe59adcdd75ade59c357ae4cb61d0231c023189
David Ledesma <ledesma@outlook.com.ar>
2023-07-04 23:36:02 -0300

    Adopcion funciona con la API ahora.

--- js/admin.js
+++ js/admin.js
@@ -1,20 +1,20 @@
 const { createApp } = Vue
-  createApp({
+createApp({
     data() {
-      return {
-        mascotas:[],
-        //url:'http://localhost:5000/productos', 
-   // si el backend esta corriendo local  usar localhost 5000(si no lo subieron a pythonanywhere)
-        url:'https://tfohuellitas.pythonanywhere.com/mascotas',   // si ya lo subieron a pythonanywhere
-        error:false,
-        cargando:true,
-        /*atributos para el guardar los valores del formulario */
-        id:0,
-        nombre:"", 
-        imagen:"",
-        edadmeses:"",
-        sexo:"",
-    }  
+        return {
+            mascotas: [],
+            //url:'http://localhost:5000/productos', 
+            // si el backend esta corriendo local  usar localhost 5000(si no lo subieron a pythonanywhere)
+            url: 'https://tfohuellitas.pythonanywhere.com/mascotas',   // si ya lo subieron a pythonanywhere
+            error: false,
+            cargando: true,
+            /*atributos para el guardar los valores del formulario */
+            id: 0,
+            nombre: "",
+            imagen: "",
+            edadmeses: "",
+            sexo: "",
+        }
     },
     methods: {
         fetchData(url) {
@@ -22,34 +22,34 @@ const { createApp } = Vue
                 .then(response => response.json())
                 .then(data => {
                     this.mascotas = data;
-                    this.cargando=false
+                    this.cargando = false
                 })
                 .catch(err => {
                     console.error(err);
-                    this.error=true              
+                    this.error = true
                 })
         },
         eliminar(id) {
-            const url = this.url+'/' + id;
+            const url = this.url + '/' + id;
             var options = {
                 method: 'DELETE',
             }
             fetch(url, options)
                 .then(res => res.text()) // or res.json()
                 .then(res => {
-			 alert('Registro Eliminado')
+                    alert('Registro Eliminado')
                     location.reload(); // recarga el json luego de eliminado el registro
                 })
         },
-        grabar(){
+        grabar() {
             let mascota = {
-                nombre:this.nombre,
+                nombre: this.nombre,
                 edadmeses: this.edadmeses,
                 sexo: this.sexo,
-                imagen:this.imagen
+                imagen: this.imagen
             }
             var options = {
-                body:JSON.stringify(mascota),
+                body: JSON.stringify(mascota),
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 redirect: 'follow'
@@ -62,10 +62,13 @@ const { createApp } = Vue
                 .catch(err => {
                     console.error(err);
                     alert("Error al Grabar")  // puedo mostrar el error tambien
-                })      
+                })
+        },
+        getImg(imagen) {
+            return './img/' + imagen;
         }
     },
     created() {
         this.fetchData(this.url)
     },
-  }).mount('#app')
+}).mount('#app')
